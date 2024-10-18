5636f2a287740d2a9167748b6441a8363afc7ddb
Abdelrahman Saleh <a.saleh2136@nu.edu.eg>
2024-10-18 10:37:34 +0300

    repositories implemented

---
+++ SchedsForums.Domain/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace SchedsForums.Domain.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException(string message) : base(message)
+        {
+        }
+
+    }
+}
---
+++ SchedsForums.Infrastructure/Repositories/Common/GenericRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchedsForums.Interface;
+using SchedsForums.Persistence.Contexts;
+
+
+namespace SchedsForums.Infrastructure.Repositories.Common
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class, IBaseEntity
+    {
+        //TODO: solve referencing issue and throw custom exception
+
+        private readonly ForumsDbContext _context;
+        private readonly DbSet<T> _dbSet;
+        public GenericRepository(ForumsDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+        public async Task<T> InsertAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> DeleteAsync(string id)
+        {
+            var entity = await _dbSet.FindAsync(id)?? throw new NullReferenceException($"Can't find an entity with this Id: {id}.");
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(string id)
+        {
+            return await _dbSet.FindAsync(id) ?? throw new NullReferenceException($"Can't find an entity with this Id: {id}.");
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
