67b9cb7ea57dde9afef304678dfa9d825766ccaf
Abdelrahman Saleh <a.saleh2136@nu.edu.eg>
2024-11-08 13:33:30 +0200

    user id, not done

--- SchedsForums.Application/Commands/Forums/GeneralForums/Create/CreateGeneralForumCommandHandler.cs
+++ SchedsForums.Application/Commands/Forums/GeneralForums/Create/CreateGeneralForumCommandHandler.cs
@@ -1,27 +1,34 @@
 ﻿using MediatR;
 using SchedsForums.Application.Interfaces.Common;
+using SchedsForums.Application.Interfaces.Services;
 using SchedsForums.Domain.Entities.Forums;
 
 namespace SchedsForums.Application.Commands.Forums.GeneralForums.Create
 {
-    public class CreateGeneralForumCommandHandler(IBaseRepository<GeneralForum> generalForum) : IRequestHandler<CreateGeneralForumCommand, CreateGeneralForumCommandResponseDTO>
+    public class CreateGeneralForumCommandHandler(IBaseRepository<GeneralForum> generalForum, ICurrentUserService currentUserService) 
+        : IRequestHandler<CreateGeneralForumCommand, CreateGeneralForumCommandResponseDTO>
     {
         private readonly IBaseRepository<GeneralForum> _generalForumRepository = generalForum 
             ?? throw new ArgumentNullException(nameof(generalForum));
+        private readonly ICurrentUserService _currentUserService = currentUserService
+            ?? throw new ArgumentNullException(nameof(currentUserService));
         public async Task<CreateGeneralForumCommandResponseDTO> Handle(CreateGeneralForumCommand request, CancellationToken cancellationToken)
         {
+            var createdById = _currentUserService.GetUserId();
             var newGeneralForum = new GeneralForum
             {
                 Title = request.Title,
                 Description = request.Description,
-                Guidelines = request.Guidelines
+                Guidelines = request.Guidelines,
+                CreatedById = createdById
             };
             await _generalForumRepository.InsertAsync(newGeneralForum);
             return new CreateGeneralForumCommandResponseDTO
             {
                 Title = newGeneralForum.Title,
                 Description = newGeneralForum.Description,
-                Guidelines = newGeneralForum.Guidelines
+                Guidelines = newGeneralForum.Guidelines,
+                CreatedById = newGeneralForum.CreatedById
             };
 
         }
--- SchedsForums.Application/Commands/Forums/GeneralForums/Create/CreateGeneralForumCommandResponseDTO.cs
+++ SchedsForums.Application/Commands/Forums/GeneralForums/Create/CreateGeneralForumCommandResponseDTO.cs
@@ -7,5 +7,6 @@ namespace SchedsForums.Application.Commands.Forums.GeneralForums.Create
         public string Title { get; set; }
         public string Description { get; set; }
         public virtual IEnumerable<string> Guidelines { get; set; }
+        public Guid CreatedById { get; set; }
     }
 }
